09b06cc9727c0750c12ac88972f0b785560632ff
João Marcelo <marcelojoao999543293@gmail.com>
2026-05-05 18:32:42 -0300

    Update main.c

--- main.c
+++ main.c
@@ -5,18 +5,22 @@
 #include "turma.h"
 
 int main() {
-    iniciar_banco();
+    menu_banco();  /* cria ou abre o banco */
 
     int op;
     while (1) {
-        printf("\n========= %s =========\n", nome_banco);
+        printf("\n======= %s =======\n", banco.nome);
         printf("1-ALUNOS  2-PROFESSORES  3-TURMAS  0-SAIR\n");
         printf("Opcao: ");
         scanf("%d", &op);
         if      (op == 1) menu_aluno();
         else if (op == 2) menu_professor();
         else if (op == 3) menu_turma();
-        else if (op == 0) { printf("Saindo...\n"); return 0; }
+        else if (op == 0) {
+            banco_salvar();
+            printf("Dados salvos. Ate logo!\n");
+            return 0;
+        }
         else printf("Invalido!\n");
     }
 }
